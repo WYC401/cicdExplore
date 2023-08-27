b69be14f72b3495b7055b5381224d1a2f0c39ef0
WYC401 <545301030@qq.com>
2023-08-27 11:42:25 -0700

    make changes to the build.sh

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 set -xe
+#gp to the app foler
+cd ~/app/VCL-content-platform-server/
 
-  # Maven is used to build  and create a war file.
-  mvn -Dmaven.test.skip=true clean install
+#clone the lastest version of the branch
+git checkout eason/docker_trial
+git pull orgin eason/docker_trial
+
+#delete the old image and create the new one
+image_id=$(docker images --format "{{.ID}}" vcl_content_platform_backend_https)
+if docker ps -a --filter "ancestor=$image_id" -q | grep -q .; then
+    # Stop and remove the running containers
+    docker ps -a --filter "ancestor=$image_id" -q | xargs -r docker stop
+    docker ps -a --filter "ancestor=$image_id" -q | xargs -r docker rm
+fi
+docker rmi "$image_id"
+docker build -t vcl_content_platform_backend_https .
+docker run -t -i -p 5000:5000 vcl_content_platform_backend_https
+
 
 
